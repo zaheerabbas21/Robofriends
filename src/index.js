be0bb0442c2e77038785c016b0a321f706a9a668
Zaheer Abbas <nk4456542@gmail.com>
2020-10-21 23:42:18 +0530

    RobotsPart1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello.js'
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
-import 'tachyons'
+import 'tachyons';
+import { robots } from './robots';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hello name={" Zaheer Abbas "} greeting={`Congrats, You are a React Developer Now.`}/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
   </React.StrictMode>,
   document.getElementById('root')
 );
