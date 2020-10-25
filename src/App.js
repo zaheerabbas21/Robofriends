1597ee582dbca5eea6ec4752b6961224df10ad29
Zaheer Abbas <nk4456542@gmail.com>
2020-10-25 18:38:25 +0530

    Robofriends Part 3 and Part 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
+// import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -25,9 +25,16 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            // robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount(){
+        // fetch is a method on window object
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -35,18 +42,22 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = robots.filter(robot => {
+        const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <Fragment>
-                <div className='tc'>
-                    <h1 className='f1'>Robofriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredRobots} />
-                </div>
-            </Fragment>
-        );
+        if(this.state.robots.length === 0){
+            return <h1 className='f1 tc'>Loading</h1>
+        }else{
+            return (
+                <Fragment>
+                    <div className='tc'>
+                        <h1 className='f1'>Robofriends</h1>
+                        <SearchBox searchChange={this.onSearchChange} />
+                        <CardList robots={filteredRobots} />
+                    </div>
+                </Fragment>
+            );
+        }
     }
 }
 
